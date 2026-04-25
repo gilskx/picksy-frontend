adf6f6ec07f696f1e686ecc6c573546257b1e5b5
gilskx <gilskx@gmail.com>
2026-04-25 01:31:33 -0500

    removed Old-Page folder

--- Old-Page/services/searchService.ts
+++
@@ -1,9 +0,0 @@
-export async function searchProducts(query: string) {
-  const res = await fetch("http://localhost:8081/search", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ query })
-  });
-
-  return res.json();
-}
